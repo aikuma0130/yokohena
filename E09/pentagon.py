6cba31a995d79db3cf316ff60e8876302fbf3180
aikuma <takahiro.kida.0130@gmail.com>
2016-11-05 20:45:37 +0900

    Resolve first

--- E09/pentagon.py
+++ E09/pentagon.py
@@ -1,15 +1,106 @@
-import re
 import unittest
-from pprint import pprint
 
 
-class Field(object):
+class Pentagon(object):
     def __init__(self):
-        self.field = [ [ 0 for i in range(100) ] for i in range(100) ]
+        self.now = [ 500, 500 ]
+        self.t = [ 0, 2 ]
+        self.tr = [ 2, 1 ]
+        self.tl = [ -2, 1 ]
+        self.dr = [ 1, -2 ]
+        self.dl = [ -1, -2 ]
+
+    def _swap(self):
+        self.t = self.t[::-1]
+        self.tr = self.tr[::-1]
+        self.tl = self.tl[::-1]
+        self.dr = self.dr[::-1]
+        self.dl = self.dl[::-1]
+
+    def _reverse(self, i):
+        self.t[i] = -self.t[i]
+        self.tr[i] = -self.tr[i]
+        self.tl[i] = -self.tl[i]
+        self.dr[i] = -self.dr[i]
+        self.dl[i] = -self.dl[i]
+
+    def move(self, direction):
+        #print(self.now)
+        if direction == 't':
+            self.now[0] += self.t[0]
+            self.now[1] += self.t[1]
+            self._reverse(0)
+            self._reverse(1)
+        elif direction == 'tr':
+            self.now[0] += self.tr[0]
+            self.now[1] += self.tr[1]
+            self._swap()
+            self._reverse(1)
+        elif direction == 'dl':
+            self.now[0] += self.dl[0]
+            self.now[1] += self.dl[1]
+            self._swap()
+            self._reverse(1)
+        elif direction == 'tl':
+            self.now[0] += self.tl[0]
+            self.now[1] += self.tl[1]
+            self._swap()
+            self._reverse(0)
+        elif direction == 'dr':
+            self.now[0] += self.dr[0]
+            self.now[1] += self.dr[1]
+            self._swap()
+            self._reverse(0)
+
+import collections
+class Field(object):
+
+    def __init__(self):
+        self.direction = collections.deque(['t','tr','dr','dl','tl'])
+        self.field = [ [None] * 1000 for i in range(1000) ]
+        self.pentagon = Pentagon()
+        self.field[self.pentagon.now[0]][self.pentagon.now[1]] = 0
+        self.move_count = 0
 
     def solve(self, inputs):
-        pass
+        for command in inputs:
+            if command == 'F':
+                self.move_count += 1
+                self.pentagon.move(self.direction[0])
+                #print('move to {0}'.format(self.direction[0]))
+                if self.is_visited(self.pentagon.now):
+                    return str(self.move_count) + ',' + str(self.field[self.pentagon.now[0]][self.pentagon.now[1]])
+                else:
+                    self.field[self.pentagon.now[0]][self.pentagon.now[1]] = self.move_count
+                    self.change_direction('t')
+            elif command == 'c':
+                self.change_direction('c')
+            elif command == 'a':
+                self.change_direction('a')
+        return '-'
 
+    def is_visited(self, now):
+        if self.field[now[0]][now[1]] == None:
+            return False
+        else:
+            return True
+
+    def change_direction(self, direction):
+        if direction == 't':
+            if self.direction[0] == 't':
+                pass
+            elif self.direction[0] == 'tr':
+                self.direction.rotate(-1)
+            elif self.direction[0] == 'tl':
+                self.direction.rotate(1)
+            elif self.direction[0] == 'dr':
+                self.direction.rotate(1)
+            elif self.direction[0] == 'dl':
+                self.direction.rotate(-1)
+        elif direction == 'a':
+            self.direction.rotate(1)
+        elif direction == 'c':
+            self.direction.rotate(-1)
 
 TEST_DATA='''FcFcFaF 3,0
 FccFaaFcFcF -
@@ -71,22 +162,20 @@ class Test(unittest.TestCase):
             field = Field()
             actual = field.solve(inputs)
             print('----TEST {0}----'.format(test_num))
+            print('inputs: {0}'.format(inputs))
             print('actual: {0}'.format(actual))
             self.assertEqual(actual, expect)
             print('OK')
         # For one testcase
         #for num, line in enumerate(TEST_DATA.split('\n')):
-        #    if test_num == 14:
+        #    if num == 12:
         #        line = line.rstrip()
-        #        inputs = line.split(' ')[0]
-        #        expect = line.split(' ')[1]
-        #        black = inputs.split(',')[0]
-        #        white = inputs.split(',')[1]
-        #        board = Board(black, white)
-        #        actual = board.solve()
-        #        print('----TEST {0}----'.format(test_num))
+        #        inputs, expect = line.split(' ')
+        #        field = Field()
+        #        actual = field.solve(inputs)
+        #        print('----TEST {0}----'.format(num))
+        #        print('inputs: {0}'.format(inputs))
         #        print('actual: {0}'.format(actual))
-        #        board.show_board()
         #        self.assertEqual(actual, expect)
         #        print('OK')
 

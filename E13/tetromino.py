3f749f167c893e5a61aa303df1ddec4a8b44a364
Dakie <ktaka0130@gmail.com>
2017-04-01 20:07:52 +0900

    first commit

---
+++ E13/tetromino.py
@@ -0,0 +1,163 @@
+import re
+import unittest 
+
+class Hexagon(self, name):
+    def __ini__(self, name):
+        self.name = name
+        self.right = chr( ord(name) + 1 )
+        self.left = chr( ord(name) - 1 )
+        self.up = chr( ord(name) - 5 )
+        self.down = chr( ord(name) + 5 )
+
+        if self.right in [ 'a', 'f', 'j', 'o', 's' ]:
+            self.right = None
+        if self.left in [ 'e', 'i', 'n', 'r', 'w' ] or self.name == 'a':
+            self.left = None
+        if self.up in [ 'e', 'n', 'w']:
+            self.up = None
+        if self.down in [ 'f', 'o', 'w'] or self.name in ['s', 't', 'u', 'v', 'w']:
+            self.down = None
+
+
+class Solver(object):
+    def __init__(self):
+        pass
+
+    def isB(self, inputs):
+        if inputs.sort() in self.correctB:
+            return True
+        else:
+            return False
+
+    def isD(self, zahyo):
+        if inputs.sort() in self.correctC:
+            return True
+        else:
+            return False
+
+    def isI(self, zahyo):
+        if inputs.sort() in self.correctI:
+            return True
+        else:
+            return False
+
+    def solve(self, inputs):
+        if self.isB(inputs):
+            return "B"
+        elif self.isD(inputs):
+            return "D"
+        elif self.isI(inputs):
+            return "I"
+        else:
+            return "-"
+
+
+TEST_DATA='''
+/*0*/ test( "glmq", "B" );    
+/*1*/ test( "fhoq", "-" );    
+/*2*/ test( "lmpr", "-" );    
+/*3*/ test( "glmp", "-" );    
+/*4*/ test( "dhkl", "-" );    
+/*5*/ test( "glpq", "D" );    
+/*6*/ test( "hlmq", "-" );    
+/*7*/ test( "eimq", "I" );    
+/*8*/ test( "cglp", "-" );    
+/*9*/ test( "chlq", "-" );    
+/*10*/ test( "glqr", "-" );    
+/*11*/ test( "cdef", "-" );    
+/*12*/ test( "hijk", "-" );    
+/*13*/ test( "kpqu", "B" );    
+/*14*/ test( "hklm", "B" );    
+/*15*/ test( "mqrw", "B" );    
+/*16*/ test( "nrvw", "B" );    
+/*17*/ test( "abfj", "B" );    
+/*18*/ test( "abcf", "B" );    
+/*19*/ test( "mrvw", "D" );    
+/*20*/ test( "ptuv", "D" );    
+/*21*/ test( "lmnr", "D" );    
+/*22*/ test( "hklp", "D" );    
+/*23*/ test( "himr", "D" );    
+/*24*/ test( "dhil", "D" );    
+/*25*/ test( "hlpt", "I" );    
+/*26*/ test( "stuv", "I" );    
+/*27*/ test( "bglq", "I" );    
+/*28*/ test( "glmn", "-" );    
+/*29*/ test( "fghm", "-" );    
+/*30*/ test( "cdgk", "-" );    
+/*31*/ test( "lpst", "-" );    
+/*32*/ test( "imrw", "-" );    
+/*33*/ test( "dinr", "-" );    
+/*34*/ test( "cdin", "-" );    
+/*35*/ test( "eghi", "-" );    
+/*36*/ test( "cdeg", "-" );    
+/*37*/ test( "bgko", "-" );    
+/*38*/ test( "eimr", "-" );    
+/*39*/ test( "jotu", "-" );    
+/*40*/ test( "kotu", "-" );    
+/*41*/ test( "lqtu", "-" );    
+/*42*/ test( "cdim", "-" );    
+/*43*/ test( "klot", "-" );    
+/*44*/ test( "kloq", "-" );    
+/*45*/ test( "kmpq", "-" );    
+/*46*/ test( "qrvw", "-" );    
+/*47*/ test( "mnqr", "-" );    
+/*48*/ test( "kopt", "-" );    
+/*49*/ test( "mnpq", "-" );    
+/*50*/ test( "bfko", "-" );    
+/*51*/ test( "chin", "-" );    
+/*52*/ test( "hmnq", "-" );    
+/*53*/ test( "nqrw", "-" );    
+/*54*/ test( "bchi", "-" );    
+/*55*/ test( "inrw", "-" );    
+/*56*/ test( "cfgj", "-" );    
+/*57*/ test( "jnpv", "-" );    
+/*58*/ test( "flmp", "-" );    
+/*59*/ test( "adpw", "-" );    
+/*60*/ test( "eilr", "-" );    
+/*61*/ test( "bejv", "-" );    
+/*62*/ test( "enot", "-" );    
+/*63*/ test( "fghq", "-" );    
+/*64*/ test( "cjms", "-" );    
+/*65*/ test( "elov", "-" );    
+/*66*/ test( "chlm", "D" );    
+/*67*/ test( "acop", "-" );    
+/*68*/ test( "finr", "-" );    
+/*69*/ test( "qstu", "-" );    
+/*70*/ test( "abdq", "-" );    
+'''
+
+
+class Test(unittest.TestCase):
+    def test_solve(self):
+        for test_num, line in enumerate(TEST_DATA.split('\n')[1:-2]):
+            test_num += 1
+            first_pattern = re.compile('^.*test\(')
+            end_pattern = re.compile('\); *')
+            line = first_pattern.sub('', line)
+            line = end_pattern.sub('', line)
+            line = line.replace(' ', '')
+            line = line.replace('"', '')
+            inputs, expect = line.split(',')
+            solver = Solver()
+            actual = solver.solve(inputs)
+            print('----TEST {0}----'.format(test_num))
+            print('inputs: {0}'.format(inputs))
+            print('actual: {0}'.format(actual))
+            self.assertEqual(actual, expect)
+            print('OK')
+        # For one testcase
+        #for test_num, line in enumerate(TEST_DATA.split('\n')):
+        #    test_num += 1
+        #    if test_num == 12:
+        #        line = line.rstrip()
+        #        inputs, expect = line.split(' ')
+        #        field = Field()
+        #        actual = field.solve(inputs)
+        #        print('----TEST {0}----'.format(test_num))
+        #        print('inputs: {0}'.format(inputs))
+        #        print('actual: {0}'.format(actual))
+        #        self.assertEqual(actual, expect)
+        #        print('OK')
+
+if __name__ == '__main__':
+    unittest.main()
